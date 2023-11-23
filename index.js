8dfea64c01f27197699ae8dbfb821169a4beeb7b
Miraj Howlader <110389768+MirajHowlader@users.noreply.github.com>
2023-11-24 01:04:22 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+
+const apiKey = 'cd4fcde5a6bc110f3c0f2311dbadc10f';
+
+const weatherDataEl = document.getElementById('weather-data');
+
+const cityInputEl = document.getElementById('city-input');
+
+const formEl = document.querySelector('form');
+
+formEl.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    const cityValue = cityInputEl.value;
+    getWeatherData(cityValue)
+   
+
+})
+async function getWeatherData(cityValue){
+    try {
+        const response =await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`)
+        
+
+        if(!response.ok){
+            throw new Error('Network response was not ok')
+        }
+        const data = await response.json()
+        
+        const temperature = Math.round(data.main.temp);
+
+        const description = data.weather[0].description;
+
+        const icon = data.weather[0].icon;
+
+        const details = [
+            `Feels like: ${ Math.round(data.main.feels_like)}`,
+            `Humidity: ${data.main.humidity}%`,
+            `Wind speed: ${data.wind.speed} m/s`,
+        ]
+        weatherDataEl.querySelector('.icon').innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="">`
+        weatherDataEl.querySelector('.tempeture').textContent = `${temperature}°C`
+        weatherDataEl.querySelector('.description').textContent = `${description}°C`
+
+
+        weatherDataEl.querySelector('.details').innerHTML = details.map((detail)=>`<div>${detail}</div>`).join('')
+        
+    } catch (error) {
+
+        weatherDataEl.querySelector('.icon').innerHTML =""
+        weatherDataEl.querySelector('.tempeture').textContent = ""
+        weatherDataEl.querySelector('.description').textContent = "An error happened, please try again and later"
+
+
+        weatherDataEl.querySelector('.details').innerHTML = ""
+    
+        
+    }
+}
